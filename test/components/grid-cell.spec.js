4098d4688c5d6503be374fff6f5cffc09b0fe870
Anthony Fung <Ant-f@users.noreply.github.com>
2018-10-29 20:58:46 +0000

    Refactor/tidy up grid cell tests

--- test/components/grid-cell.spec.js
+++ test/components/grid-cell.spec.js
@@ -6,22 +6,6 @@ import Enzyme from '../root-hooks.spec.js';
 import GridCell from '../../src/components/grid-cell.jsx';
 
 const getWrapper = props => {
-  if (props.adjacentTrapCount === undefined) {
-    props.adjacentTrapCount = 0;
-  }
-  
-  if (props.isGameOver === undefined) {
-    props.isGameOver = false;
-  }
-  
-  if (props.isRevealed === undefined) {
-    props.isRevealed = false;
-  }
-  
-  if (props.isTrap === undefined) {
-    props.isTrap = false;
-  }
-
   return Enzyme.shallow(
     <GridCell
       adjacentTrapCount={props.adjacentTrapCount}
@@ -32,37 +16,52 @@ const getWrapper = props => {
 };
 
 describe('<GridCell/>', function () {
+  let props = {
+    adjacentTrapCount: 0,
+    isGameOver: false,
+    isRevealed: false,
+    isTrap: false
+  };
+
   describe('When the game is over', function () {
+    this.beforeEach(function () {
+      props = {...props, isGameOver: true};
+    });
+
     it('Shows trap when present', function () {
-      const wrapper = getWrapper({ isGameOver: true, isTrap: true });
+      const wrapper = getWrapper({ ...props, isTrap: true });
       const span = wrapper.find('span');
       expect(span.text().trim()).to.equal('T');
     });
 
     it('Does not show trap when not present', function () {
-      const wrapper = getWrapper({ isGameOver: true, isTrap: false });
+      const wrapper = getWrapper({ ...props, isTrap: false });
       const spans = wrapper.find('span');
       expect(spans).to.have.lengthOf(0);
     });
   });
 
   describe('When the game is not over', function () {
+    this.beforeEach(function () {
+      props = { ...props, isGameOver: false };
+    });
+
     it('Does not show trap when present', function () {
-      const wrapper = getWrapper({ isGameOver: false, isTrap: true });
+      const wrapper = getWrapper({ ...props, isTrap: true });
       const spans = wrapper.find('span');
       expect(spans).to.have.lengthOf(0);
     });
 
     it('Does not show trap when not present', function () {
-      const wrapper = getWrapper({ isGameOver: false, isTrap: false });
+      const wrapper = getWrapper({ ...props, isTrap: false });
       const spans = wrapper.find('span');
       expect(spans).to.have.lengthOf(0);
     });
 
     it('Does not show adjacent trap count when not revealed', function () {
       const wrapper = getWrapper({
+        ...props,
         adjacentTrapCount: 3,
-        isGameOver: false,
         isRevealed: false,
         isTrap: false
       });
@@ -72,27 +71,19 @@ describe('<GridCell/>', function () {
     });
 
     describe('When revealed', function () {
+      this.beforeEach(function () {
+        props = { ...props, isRevealed: true };
+      });
+
       it('Shows adjacent trap count', function () {
-        const wrapper = getWrapper({
-          adjacentTrapCount: 3,
-          isGameOver: false,
-          isRevealed: true,
-          isTrap: false
-        });
-          
+        const wrapper = getWrapper({ ...props, adjacentTrapCount: 3, isTrap: false });
         const span = wrapper.find('span');
         const trapCount = Number(span.text().trim());
         expect(Number.isInteger(trapCount)).to.be.true;
       });
         
       it('Does not show adjacent trap count when 0', function () {
-        const wrapper = getWrapper({
-          adjacentTrapCount: 0,
-          isGameOver: false,
-          isRevealed: true,
-          isTrap: false
-        });
-          
+        const wrapper = getWrapper({ ...props, adjacentTrapCount: 0, isTrap: false });
         const spans = wrapper.find('span');
         expect(spans).to.have.lengthOf(0);
       });
